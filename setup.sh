7bb8cacf2264c5ada7aabbb0141f5017ab2eba2d
kawau1 <119904016+kawau1@users.noreply.github.com>
2023-09-30 23:49:30 +0900

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# https://brew.sh/ja/
+# Homebrewのインストール後に実行する
+
+brew install ffmpeg
+brew install yt-dlp
+brew install --cask visual-studio-code
+brew install --cask notion
+brew install --cask sketch
+brew install --cask steam
+brew install --cask discord
