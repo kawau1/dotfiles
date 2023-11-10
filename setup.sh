e4f2a66ff86dff00bc1eff2b466856bde5cd76bd
kawau1 <119904016+kawau1@users.noreply.github.com>
2023-11-10 20:41:58 +0900

    add

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 
 brew install ffmpeg
 brew install yt-dlp
+brew install gh
+# gh auth login
+# gh extension install github/gh-copilot
 brew install --cask visual-studio-code
 brew install --cask notion
 brew install --cask sketch
